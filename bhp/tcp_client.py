955ed2327df9657d728f9c2302f2133bf92b7284
sasrio <robot@sasr.io>
2015-08-06 00:33:01 +0200

    update bhp until tcp_server

--- bhp/tcp_client.py
+++ bhp/tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9999
 
 # client object,
 # AF_INET for iPv4 or hostname,
@@ -9,7 +9,7 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
-data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+data = "hi"
 client.send(data)
 
 response = client.recv(4096)
